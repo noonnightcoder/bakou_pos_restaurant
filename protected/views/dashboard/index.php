d73460e0feec2a121c43faf299ebceb39fa5fb24
noonnightcoder <noonnightcoder@gmail.com>
2017-04-22 06:07:41 +0700

    general bug fix

--- protected/views/dashboard/index.php
+++ protected/views/dashboard/index.php
@@ -19,6 +19,7 @@ $this->breadcrumbs=array(
 
 <div class="">
         <div class="row">
+
             <!--PAGE CONTENT BEGINS-->
             <div class="col-xs-12">
 
@@ -30,103 +31,11 @@ $this->breadcrumbs=array(
                     'total' => $total,
                 )); ?>
 
-
                 <div class="space-8"></div>
 
-                <div class="row">
-                    <div class="col-xs-12 col-sm-6 widget-container-col">
-                        <div class="widget-box widget-color-blue2">
-                            <div class="widget-header">
-                                <h5 class="widget-title bigger lighter">
-                                    <i class="ace-icon fa fa-trophy"></i>
-                                    <?php echo Yii::t('app','This Year Top 10 Food ') . Yii::t('app','Ranked by QUANTITY'); ?>
-                                </h5>
-                            </div>
-
-                            <div class="widget-body">
-                                <div class="widget-main no-padding">
-
-                                    <?php $this->widget('yiiwheels.widgets.grid.WhGridView',array(
-                                        'id'=>'top-product-grid-qty',
-                                        'fixedHeader' => true,
-                                        'responsiveTable' => true,
-                                        'type'=>TbHtml::GRID_TYPE_BORDERED,
-                                        'dataProvider'=>$report->dashtopFood(),
-                                        //'summaryText' =>'<p class="text-info" align="left">' . Yii::t('app','This Year Top 10 Food ') . Yii::t('app','Ranked by QUANTITY') .'</p>',
-                                        'summaryText' => '',
-                                        'columns'=>array(
-                                            array('name'=>'rank',
-                                                'header'=>Yii::t('app','Rank'),
-                                                'value'=>'$data["rank"]',
-                                            ),
-                                            array('name'=>'item_name',
-                                                'header'=>Yii::t('app','Item Name'),
-                                                'value'=>'$data["item_name"]',
-                                            ),
-                                            array('name'=>'qty',
-                                                'header'=>Yii::t('app','Quantity'),
-                                                'value'=>'number_format($data["qty"],Yii::app()->shoppingCart->getDecimalPlace())',
-                                                //'footer'=>$report->paymentTotalQty() ,
-                                            ),
-                                            array('name'=>'amount',
-                                                'header'=>Yii::t('app','Amount'),
-                                                'value'=>'number_format($data["amount"],Yii::app()->shoppingCart->getDecimalPlace())',
-                                                //'footer'=>Yii::app()->getNumberFormatter()->formatCurrency($report->paymentTotalAmount(),'USD'),
-                                            ),
-                                        ),
-                                    )); ?>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xs-12 col-sm-6 widget-container-col">
-                        <div class="widget-box widget-color-blue2">
-                            <div class="widget-header">
-                                <h5 class="widget-title bigger lighter">
-                                    <i class="ace-icon fa fa-trophy"></i>
-                                    <?php echo Yii::t('app','This Year Top 10 Beverage ') . Yii::t('app','Ranked by QUANTITY'); ?>
-                                </h5>
-
-                            </div>
-                            <div class="widget-body">
-                                <div class="widget-main no-padding">
-                                    <?php $this->widget('yiiwheels.widgets.grid.WhGridView',array(
-                                        'id'=>'top-product-grid-amount',
-                                        'fixedHeader' => true,
-                                        'responsiveTable' => true,
-                                        'type'=>TbHtml::GRID_TYPE_BORDERED,
-                                        'dataProvider'=>$report->dashtopBeverage(),
-                                        //'summaryText' =>'<p class="text-info" align="left">' . Yii::t('app','This Year Top 10 Beverage ') . Yii::t('app','Ranked by QUANTITY') .'</p>',
-                                        'summaryText' => '',
-                                        'columns'=>array(
-                                            array('name'=>'rank',
-                                                'header'=>Yii::t('app','Rank'),
-                                                'value'=>'$data["rank"]',
-                                            ),
-                                            array('name'=>'item_name',
-                                                'header'=>Yii::t('app','Item Name'),
-                                                'value'=>'$data["item_name"]',
-                                            ),
-                                            array('name'=>'qty',
-                                                'header'=>Yii::t('app','Quantity'),
-                                                'value'=>'number_format($data["qty"],0)',
-                                                //'footer'=>$report->paymentTotalQty() ,
-                                            ),
-                                            array('name'=>'amount',
-                                                'header'=>Yii::t('app','Amount'),
-                                                'value'=>'number_format($data["amount"],Yii::app()->shoppingCart->getDecimalPlace())',
-                                                //'footer'=>Yii::app()->getNumberFormatter()->formatCurrency($report->paymentTotalAmount(),'USD'),
-                                            ),
-                                        ),
-                                    )); ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
+                <?php $this->renderPartial('partial/widget_topten', array(
+                    'report' => $report,
+                )); ?>
 
             </div><!--/row-->
         </div>
